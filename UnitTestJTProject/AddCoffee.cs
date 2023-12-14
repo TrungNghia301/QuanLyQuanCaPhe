834ea359353f701a2662b7d763400be2d9c849c1
TrungNghia301 <nghiatrung.swd.301@gmail.com>
2023-12-14 16:28:45 +0700

    Creat unit test add coffee

---
+++ UnitTestJTProject/AddCoffee.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PresentLayer;
+using System;
+
+namespace UnitTestJTProject
+{
+    [TestClass]
+    public class AddCoffee
+    {
+        private frmThemHang frmthemhang;
+        [TestInitialize]
+        public void Initialize()
+        {
+            frmthemhang = new frmThemHang();
+        }
+        [TestMethod]
+        // Test add coffee with the same name Latte
+        public void TestAddCoffee()
+        {
+            frmthemhang.AddCoffee("Latte", 1000000);
+        }
+    }
+}
