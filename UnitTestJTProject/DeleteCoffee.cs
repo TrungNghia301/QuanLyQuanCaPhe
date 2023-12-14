67ffd91b172dce457d2a190c6cf91b29ef3da767
TrungNghia301 <nghiatrung.swd.301@gmail.com>
2023-12-14 20:18:55 +0700

    Create unit test for deleteCofee

---
+++ UnitTestJTProject/DeleteCoffee.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PresentLayer;
+using System;
+
+namespace UnitTestJTProject
+{
+    [TestClass]
+    public class DeleteCoffee
+    {
+        private frmThemHang frmthemhang;
+        private frmManage frmmanage;
+        [TestInitialize]
+        public void Initialize()
+        {
+            frmthemhang = new frmThemHang();
+            frmthemhang.AddCoffee("CoffeetoDelete", 0, DateTime.Now, DateTime.Now.AddDays(20), 0, 0, "");
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
