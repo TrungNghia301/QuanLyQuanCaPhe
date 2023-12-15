b9da637bdab237a9716c01f7bb22190164ab47d6
TrungNghia301 <nghiatrung.swd.301@gmail.com>
2023-12-15 17:33:06 +0700

    Complete unit test Delete user

--- UnitTestJTProject/DeleteUser.cs
+++ UnitTestJTProject/DeleteUser.cs
@@ -15,12 +15,21 @@ namespace UnitTestJTProject
         }
         [TestMethod]
         // Test corret delete user
+        // TS : create user wwith id "N20" to test delete
         public void TestDeleteUserCorrect()
         {
-            frmmanage.AddNV("N20", "Nhân viên để xóa", ".", "0", "0", 0, "0");
-            string id;
-            bool result = frmmanage.DeleteUser(id);
+            frmmanage.AddNV("N20", "Nhân viên để xóa", ".", "0", "0", 0, "testAccount");
+            string id = "N20";
+            bool result = frmmanage.DeleteNV(id);
             Assert.IsTrue(result);
+        }
+        [TestMethod]
+        // Test delete user with blank value
+        public void TestBlankDeleteUser()
+        {
+            string id = "";
+            bool result = frmmanage.DeleteNV(id);
+            Assert.IsFalse(result);
         }
     }
 }
