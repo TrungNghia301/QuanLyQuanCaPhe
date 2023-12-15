89a58c3a51ed36cb0f7d127936fdc5c47fe774df
TrungNghia301 <nghiatrung.swd.301@gmail.com>
2023-12-15 18:28:18 +0700

    Create unit test for add Account , not complete CreateBill

--- UnitTestJTProject/CreateBill.cs
+++ UnitTestJTProject/CreateBill.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PresentLayer;
 using System;
 
 namespace UnitTestJTProject
@@ -6,9 +7,26 @@ namespace UnitTestJTProject
     [TestClass]
     public class CreateBill
     {
+        private frmHome frmhome;
+        private frmThemHang frmthemhang;
+        private frmManage frmmanage;
+        [TestInitialize]
+        public void Initialize()
+        {
+            frmhome = new frmHome();
+            frmthemhang = new frmThemHang();
+            frmmanage = new frmManage();
+        }
         [TestMethod]
+        // Test Coffee with name "BlackBrown" have in store to create bill
         public void TestMethod1()
         {
+            //add "BlackBrown" to db to test
+            string coffeeName = "BlackBrown";
+            frmthemhang.AddCoffee(coffeeName, 100, DateTime.Now, DateTime.Now.AddDays(30), 12000, 35000, "");
+            //Test
+            bool result = frmhome.CreateBill()
+            frmmanage.DeleteCoffee(null, "BlackBrown");
         }
     }
 }
