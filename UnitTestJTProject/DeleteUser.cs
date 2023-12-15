ec70a11a341f91db26ac669f826ab0e76a1b8f56
TrungNghia301 <nghiatrung.swd.301@gmail.com>
2023-12-15 17:17:19 +0700

    Create unit test for Delete user

---
+++ UnitTestJTProject/DeleteUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PresentLayer;
+using System;
+
+namespace UnitTestJTProject
+{
+    [TestClass]
+    public class DeleteUser
+    {
+        private frmManage frmmanage;
+        [TestInitialize]
+        public void Initialize()
+        {
+            frmmanage = new frmManage();
+        }
+        [TestMethod]
+        // Test corret delete user
+        public void TestDeleteUserCorrect()
+        {
+            frmmanage.AddNV("N20", "Nhân viên để xóa", ".", "0", "0", 0, "0");
+            string id;
+            bool result = frmmanage.DeleteUser(id);
+            Assert.IsTrue(result);
+        }
+    }
+}
